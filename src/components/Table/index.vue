fb4762963dc95ed780f71c76061083d0097f850e
weibo_feng@arcplus.com.cn <weibo_feng@arcplus.com.cn>
2024-08-08 11:30:33 +0800

    commit

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,12 +1,14 @@
 <template>
   <el-table
-    :data="tableData"
+    :data="props.tableData"
+    stripe
     border
     :cell-style="{ textAlign: 'center' }"
     :header-cell-style="{ textAlign: 'center' }"
   >
     <el-table-column
-      v-for="item in tableColumn"
+      v-for="item in props.tableColumn"
+      :prop="item.prop"
       :type="item.type"
       :label="item.label"
       :width="item.width"
@@ -15,15 +17,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const tableData = ref([]);
-const tableColumn = ref([
-  { type: "index", label: "序号", width: "65" },
-  { prop: "S_ProjectName", label: "发布单位" },
-  { prop: "S_ProjectNo", label: "团队标准", width: "200" },
-  { prop: "S_ProjectNo", label: "应用工程", width: "200" },
-  { prop: "S_ProjectNo", label: "报建编号", width: "200" },
-]);
+import { defineProps } from "vue";
+const props = defineProps<{
+  tableData: any[];
+  tableColumn: any[];
+}>();
 </script>
 
 <style></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -25,7 +25,10 @@
         </el-collapse>
       </el-col>
       <el-col :span="16">
-        <tableData></tableData>
+        <TableData
+          :table-column="tableColumn"
+          :table-data="tableData"
+        ></TableData>
       </el-col>
       <el-col :span="4">
         <el-collapse v-model="activeNames" @change="handleChange">
@@ -58,13 +61,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const activeNames = ref<string[]>(["1"]);
-import tableData from "@/components/Table/index.vue";
+import TableData from "@/components/Table/index.vue";
 const handleChange = (val: string[]) => {
   activeNames.value = val;
 };
 const collapseItemList = ref([
   {
-    title: "一级标题",
+    title: "标准等级（数量）",
     name: "1",
     children: [
       { title: "选项1", checked: false },
@@ -73,8 +76,26 @@ const collapseItemList = ref([
     ],
   },
   {
-    title: "二级标题",
+    title: "标准纵向分级(数量)",
     name: "2",
+    children: [
+      { title: "选项4", checked: false },
+      { title: "选项5", checked: false },
+      { title: "选项6", checked: false },
+    ],
+  },
+  {
+    title: "基础分类(数量)",
+    name: "3",
+    children: [
+      { title: "选项4", checked: false },
+      { title: "选项5", checked: false },
+      { title: "选项6", checked: false },
+    ],
+  },
+  {
+    title: "专项分类(数量)",
+    name: "4",
     children: [
       { title: "选项4", checked: false },
       { title: "选项5", checked: false },
@@ -105,6 +126,33 @@ const rankCollapseItemList = ref([
 const handleCheck = (data: any) => {
   console.log(data);
 };
+const tableColumn = ref([
+  { type: "index", label: "序号", width: "65" },
+  { prop: "S_ProjectName", label: "标准名称" },
+  { prop: "S_ProjectNo", label: "标准编号", width: "200" },
+  { prop: "S_RealeaseTime", label: "发布日期", width: "200" },
+  { prop: "S_DoTime", label: "实施日期", width: "200" },
+]);
+const tableData = ref([
+  {
+    S_ProjectName: "标准名称1",
+    S_ProjectNo: "标准编号1",
+    S_RealeaseTime: "2021-09-01",
+    S_DoTime: "2021-09-01",
+  },
+  {
+    S_ProjectName: "标准名称2",
+    S_ProjectNo: "标准编号2",
+    S_RealeaseTime: "2021-09-01",
+    S_DoTime: "2021-09-01",
+  },
+  {
+    S_ProjectName: "标准名称3",
+    S_ProjectNo: "标准编号3",
+    S_RealeaseTime: "2021-09-01",
+    S_DoTime: "2021-09-01",
+  },
+]);
 </script>
 
 <style lang="scss" scoped>
