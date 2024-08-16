b1036ba1317af0b478890b1aaa617ce075208031
weibo_feng@arcplus.com.cn <weibo_feng@arcplus.com.cn>
2024-08-16 18:09:25 +0800

    commit

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container row justify-center align-middle">
-    <div style="width: 1236px" class="row align-middle space-between">
+    <div
+      @click="logout"
+      style="width: 1236px"
+      class="row align-middle space-between"
+    >
       <img src="@/assets/imgs/logo.png" style="width: 53px" class="my-mr-10" />
       <h1 style="font-size: 24px; font-weight: bolder; color: #fff">
         上海市工程建设标准体系查询系统
@@ -14,6 +18,11 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const logout = () => {
+  router.push("/nav");
+};
 export default defineComponent({
   name: "",
   setup() {
