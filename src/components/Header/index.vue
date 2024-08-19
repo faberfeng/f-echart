d30128c9d8e7996cf530b9cb0cc64521726f704c
weibo_feng@arcplus.com.cn <weibo_feng@arcplus.com.cn>
2024-08-19 09:51:12 +0800

    commit

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,34 +1,23 @@
 <template>
   <div class="container row justify-center align-middle">
-    <div
-      @click="logout"
-      style="width: 1236px"
-      class="row align-middle space-between"
-    >
-      <img src="@/assets/imgs/logo.png" style="width: 53px" class="my-mr-10" />
+    <div style="width: 1236px" class="row align-middle space-between">
+      <img src="@/assets/imgs/logo.png" style="width: 50px" class="my-mr-10" />
       <h1 style="font-size: 24px; font-weight: bolder; color: #fff">
         上海市工程建设标准体系查询系统
       </h1>
-      <!-- <el-button type="text" size="medium" style="color: #fff">
-        退出
-      </el-button> -->
     </div>
+    <div style="color: #fff; cursor: pointer" @click="logout()">退出</div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { defineComponent } from "vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
 const logout = () => {
+  console.log("logout111");
   router.push("/nav");
 };
-export default defineComponent({
-  name: "",
-  setup() {
-    return {};
-  },
-});
 </script>
 
 <style lang="scss" scoped>
